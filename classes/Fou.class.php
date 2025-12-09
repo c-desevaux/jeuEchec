0d9d2fd19704e79a136ce1c96c0f6fd7ab8dee8a
cdesevaux <corentin.desevaux@gmail.com>
2025-12-09 15:15:41 +0100

    class piece fou

--- classes/Fou.class.php
+++ classes/Fou.class.php
@@ -1,3 +1,27 @@
 <?php
 
+    class Fou extends PieceEchecs{
+
+        public function __construct(){
+            parent::__construct();
+
+            
+
+        }
+
+        public function canGo($x, $y){
+            $x0 = $this->getX();
+            $y0 = $this->getY();
+
+            if( abs($x0 - $x)/abs($y0 - $y) == 1){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    
+
+
+
 ?>
--- index.php
+++ index.php
@@ -4,14 +4,13 @@
     require_once 'classes/PieceEchecs.class.php';
     require_once 'classes/InvalidArgumentPiece.class.php';
 
-    $fou = new PieceEchecs (4,4, true);
-    var_dump($fou);
+    
 
 
     try {
        
-        // $test5 = new PieceEchecs(0, 1, true);
-        // $test6 = new PieceEchecs(1, 0, true);
+         $test5 = new PieceEchecs(0, 1, true);
+         $test6 = new PieceEchecs(1, 0, true);
 
        
     } catch (InvalidArgumentPiece $e) {
